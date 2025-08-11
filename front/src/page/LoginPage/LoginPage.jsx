65984fcfb8a7a7d4659c5c5521ae0be229431c58
networkSocerer <gigattfyhig@gmail.com>
2025-08-11 17:00:07 +0900

    update

--- front/src/page/LoginPage/LoginPage.jsx
+++ front/src/page/LoginPage/LoginPage.jsx
@@ -1,19 +1,29 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginWithGoogle } from "../../features/user/userSlice";
+import { useNavigate } from "react-router";
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-const handleGoogleLogin = async (googleData) => {
-  const dispatch = useDispatch();
-  //구글 로그인 하기
-  dispatch(loginWithGoogle(googleData.credential));
-};
 const LoginPage = () => {
-  console.log("Login Page google api id ", GOOGLE_CLIENT_ID);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, loginError } = useSelector((state) => state.user);
+  const handleGoogleLogin = async (googleData) => {
+    dispatch(loginWithGoogle(googleData.credential));
+  };
+  if (user) {
+    navigate("/");
+  }
+
   return (
     <div>
       <h1>Challenger</h1>
+      {loginError && (
+        <div className="error-message">
+          <Alert variant="danger">{loginError}</Alert>
+        </div>
+      )}
       <div className="card">
         <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
           <GoogleLogin
