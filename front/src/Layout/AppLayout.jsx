3ca4d9abdadc97826fc1561ea6e05087cc258725
networkSocerer <gigattfyhig@gmail.com>
2025-08-11 17:28:53 +0900

    update

--- front/src/Layout/AppLayout.jsx
+++ front/src/Layout/AppLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router";
-
+import "./style/App.css";
 const AppLayout = ({ children }) => {
   const location = useLocation();
   const dispatch = useDispatch();
