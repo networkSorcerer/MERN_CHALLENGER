4ed755a3893b47260bcdc48e7f84f8615200e1cb
networkSocerer <gigattfyhig@gmail.com>
2025-08-11 16:26:23 +0900

    update

--- front/src/features/store.jsx
+++ front/src/features/store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./user/userSlice";
+import userSlice from "./user/userSlice.jsx";
 
 const store = configureStore({
   reducer: {
