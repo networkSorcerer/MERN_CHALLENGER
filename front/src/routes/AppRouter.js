1535ae99b8554f10d1c599b56f87c9acafc1605f
networkSocerer <gigattfyhig@gmail.com>
2025-08-11 15:50:17 +0900

    update

--- front/src/routes/AppRouter.js
+++ front/src/routes/AppRouter.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Routes } from "react-router";
+import LoginPage from "../page/LoginPage/LoginPage";
 
 const AppRouter = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
