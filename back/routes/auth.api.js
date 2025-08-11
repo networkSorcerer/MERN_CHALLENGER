81f488bf61a0277e2ea482dbf9bcf80c1920d453
networkSocerer <gigattfyhig@gmail.com>
2025-08-11 15:33:23 +0900

    update

--- back/routes/auth.api.js
+++ back/routes/auth.api.js
@@ -2,3 +2,4 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/auth.controller");
 router.post("/google", authController.loginWithGoogle);
+module.exports = router;
