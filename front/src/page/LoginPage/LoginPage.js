d03d3495a348ee1110e2b91743b2714dc746504e
networkSocerer <gigattfyhig@gmail.com>
2025-08-11 16:18:47 +0900

    update

--- front/src/page/LoginPage/LoginPage.js
+++ front/src/page/LoginPage/LoginPage.js
@@ -1,22 +1,21 @@
-import React from "react";
-import "./style/App.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+import { GoogleLogin } from "@react-oauth/google";
 
+const handleGoogleLogin = async (googleData) => {
+  //구글 로그인 하기
+  dispatch(loginWithGoogle(googleData.credential));
+};
 const LoginPage = () => {
   return (
     <div>
       <h1>Challenger</h1>
       <div className="card">
-        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-          <GoogleLogin
-            onSuccess={handleGoogleLogin}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-        </GoogleOAuthProvider>
-      </div>{" "}
+        <GoogleLogin
+          onSuccess={handleGoogleLogin}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </div>
     </div>
   );
 };
