d91204acd00975d3e2f04550a3a4afe28fca79ad
networkSocerer <gigattfyhig@gmail.com>
2025-08-12 16:28:49 +0900

    update

--- front/src/routes/AppRouter.jsx
+++ front/src/routes/AppRouter.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import LoginPage from "../page/LoginPage/LoginPage.jsx";
 import LandingPage from "../page/LandingPage/LandingPage.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/main" element={<LandingPage />} />
+      <Route element={<PrivateRoute permissionLevel="admin" />}></Route>
     </Routes>
   );
 };
