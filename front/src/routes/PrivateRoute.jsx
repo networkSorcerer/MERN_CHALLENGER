1ff7695c5d28ef3ee3bd3475304c50b01a1ffc26
networkSocerer <gigattfyhig@gmail.com>
2025-08-12 17:49:17 +0900

    update

--- front/src/routes/PrivateRoute.jsx
+++ front/src/routes/PrivateRoute.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router";
 
 const PrivateRoute = ({ permissionLevel }) => {
   const user = useSelector((state) => state.user.user);
-  console.log("useruseruser", user);
+  useEffect(() => {
+    console.log("useruseruser", user);
+  });
   const isAuthenticated =
     user?.auth === permissionLevel || user?.auth === "admin";
 
