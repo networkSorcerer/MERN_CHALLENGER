838ba24a00c78989f602391fe00582fc0283057b
networkSocerer <gigattfyhig@gmail.com>
2025-08-12 20:16:53 +0900

    update

--- front/src/theme.jsx
+++ front/src/theme.jsx
@@ -1,24 +1,25 @@
 import { createTheme } from "@mui/material";
+import React from "react";
 
- const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: mode,
-          background: {
-            default: mode === "light" ? "#fff" : "#121212",
-          },
-          text: {
-            primary: mode === "light" ? "#000" : "#fff",
-          },
+const theme = React.useMemo(
+  () =>
+    createTheme({
+      palette: {
+        mode: mode,
+        background: {
+          default: mode === "light" ? "#fff" : "#121212",
         },
-        typography: {
-          h1: {
-            color: mode === "light" ? "#b71c1c" : "#ff7961", // 붉은 계열 예시
-          },
+        text: {
+          primary: mode === "light" ? "#000" : "#fff",
         },
-      }),
-    [mode]
-  );
+      },
+      typography: {
+        h1: {
+          color: mode === "light" ? "#b71c1c" : "#ff7961", // 붉은 계열 예시
+        },
+      },
+    }),
+  [mode]
+);
 
 export default theme;
