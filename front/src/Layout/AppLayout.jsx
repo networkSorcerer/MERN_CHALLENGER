63be92d3c4c99d4b1a4bf8f54ed6f8def3c3dfc1
networkSocerer <gigattfyhig@gmail.com>
2025-08-12 18:14:44 +0900

    update

--- front/src/Layout/AppLayout.jsx
+++ front/src/Layout/AppLayout.jsx
@@ -1,55 +1,19 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useLocation, Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
 import "./style/App.css";
-
 const AppLayout = ({ children }) => {
   const location = useLocation();
   const dispatch = useDispatch();
-  const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
+  // useEffect(() => {
+  //   const token = sessionStorage.getItem("token");
+  //   if (token) {
+  //     dispatch(loginWithToken());
+  //   }
+  // }, []);
 
-  const toggleDrawer = () => setShow(!show);
-
-  return (
-    <div>
-      {/* 상단 Navbar */}
-      <nav>
-        <div>
-          <button type="button" onClick={toggleDrawer}>
-            ☰ Menu
-          </button>
-        </div>
-      </nav>
-
-      {/* Offcanvas 사이드 메뉴 */}
-      <div
-        className={`offcanvas offcanvas-start ${show ? "show" : ""}`}
-        tabIndex="-1"
-        style={{ visibility: show ? "visible" : "hidden" }}
-      >
-        <div>
-          <h5>Menu</h5>
-          <button type="button" onClick={toggleDrawer}></button>
-        </div>
-        <div>
-          <ul>
-            <li>
-              <button onClick={() => console.log("Go Home")}>Home</button>
-            </li>
-            <li>
-              <button onClick={() => console.log("Settings")}>Settings</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      {/* 메인 컨텐츠 */}
-      <main>
-        {children}
-        <Outlet />
-      </main>
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default AppLayout;
