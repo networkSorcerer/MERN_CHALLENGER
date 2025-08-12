9c6d83e8c5c8f8f554a47e80408a6c48449d192e
networkSocerer <gigattfyhig@gmail.com>
2025-08-12 18:13:08 +0900

    update

--- front/src/routes/PrivateRoute.jsx
+++ front/src/routes/PrivateRoute.jsx
@@ -10,7 +10,7 @@ const PrivateRoute = ({ permissionLevel }) => {
   const isAuthenticated =
     user?.auth === permissionLevel || user?.auth === "admin";
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
