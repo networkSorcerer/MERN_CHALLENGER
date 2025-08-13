62050c08cf295579a0c15aebdebfe335a24d7fb9
networkSocerer <gigattfyhig@gmail.com>
2025-08-13 17:40:37 +0900

    update

--- back/routes/class.api.js
+++ back/routes/class.api.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const classController = require("../controller/class.controller");
+const authController = require("../controller/auth.controller");
+
 router.post("/", authController.authenticate, classController.addClass);
 
 module.exports = router;
